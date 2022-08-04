4ccbd0007d505f25418c25d4d42e1ce003829550
lvran <1396833166@qq.com>
2022-08-04 15:58:13 +0800

    test

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Range\Test\Index;
+
+require "vendor/autoload.php";
+
+$index = new Index();
+
+$index->index();
---
+++ src/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Range\Test;
+
+class Index
+{
+    public function index()
+    {
+        echo '如果看到我，说明你成功了';
+    }
+}
